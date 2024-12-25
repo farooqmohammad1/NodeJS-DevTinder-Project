393877b1504454f8c7996387a48beaa8ed084cd3
farooqmohammad1 <farooqmohammad613@gmail.com>
2024-12-25 11:53:17 +0530

    completed basic routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const app= express();
 
-app.use('/',(req,res)=>{
-      res.send('Hi hello')
+// app.use("/",(req,res)=>{
+//     console.log(req.params)
+//       res.send('Hi hello')
+// })
+app.use("/hello/:id",(req,res)=>{
+   console.log(req.params)
+   res.send("This is hello router")
+})
+app.use("/test/:id/:name",(req,res)=>{
+   console.log(req.params)
+   res.send("This is tests router")
+})
+app.post("/post",(req,res)=>{
+   res.send("Post req is done")
 })
 
 
 
 
-app.listen('8080',(req,res)=>{
+
+app.listen('8080',()=>{
    console.log("Hi server stared")
 })
